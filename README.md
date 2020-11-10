3496daadb8a12d1ecf39ffff30a248c86204690b
Lane Garner <b.lane.garner@gmail.com>
2020-11-10 11:33:25 -0600

    assignment complete w/ stretch

--- README.md
+++ README.md
@@ -15,7 +15,7 @@
   * deposit(amt) - This method takes in a single input, the deposit amount. This method should create a new transaction representing the deposit, and add it to the transactions array.
     * NOTE: You should not be able to deposit a negative amount
 charge
-  * (payee, amt) - This method takes in the payee and amount, creates a new transaction with the payee and amount, and adds the transaction to the transaction array.
+  * charge(payee, amt) - This method takes in the payee and amount, creates a new transaction with the payee and amount, and adds the transaction to the transaction array.
   * NOTE: You should not be able to charge an amount that would make your balance dip below 0
 
 ## Transaction class - This class represents a single transaction in a bank account.
--- app.js
+++ app.js
@@ -4,6 +4,7 @@ class BankAccount {
         this.owner = owner;
         this.transactions = [];
     }
+    
     deposit(amt) {
         if(amt <= 0) {
             return false
@@ -11,9 +12,17 @@ class BankAccount {
             return this.transactions.push(amt)
         }
     }
-    // charge(payee, amt) {
-        
-    // }
+
+    charge(amt, payee) {
+        if((this.balance() - amt) < 0) {
+            console.log('Cannot complete charge')
+            return false
+        } else {
+            new Transaction(amt, payee)
+            return this.transactions.push(amt *= -1) 
+        }
+    }
+
     balance() {
         const reducer = (acc, cur) => acc + cur;
         return this.transactions.reduce(reducer)
@@ -28,12 +37,40 @@ class Transaction {
     }
 }
 
-const myAccount = new BankAccount ('12345', 'Lane')
+class SavingsAccount extends BankAccount {
+    constructor(accountNumber, owner, interestRate){
+        super(accountNumber, owner);
+        this.interestRate= interestRate; 
+    }
+    // * accrueInterest() - This method should use the balance() to get the current balance and add a new transaction representing a deposit of the appropriate amount.
+    accrueInterest() {
+        let balance = this.balance()
+        balance = balance * this.interestRate
+        return this.deposit(balance)
+    }
+}
 
-myAccount.deposit(200)
-console.log(myAccount.balance())
+const myAccount = new BankAccount (12345, 'Lane')
 
-const buyPizza = new Transaction (40, 'Possum Pizza')
+myAccount.deposit(10)
+console.log('balance after deposit: ', myAccount.balance())
 
-console.log(buyPizza)
+myAccount.charge(30, 'possum pizza')
+console.log('balance after charge: ', myAccount.balance())
+
+
+const newSavings = new SavingsAccount (54321, 'Brian', .02)
+
+newSavings.deposit(100)
+console.log('Savings balance after deposit: ', newSavings.balance())
+
+newSavings.accrueInterest(100)
+console.log('Savings balance after interest: ', newSavings.balance())
+
+console.log('savings transactions: ', newSavings.transactions)
+
+// newSavings.charge(30, 'lil nonas')
+// console.log('balance after charge: ', newSavings.balance())
+
+
 
